58821ab7f1a1097c2be145ab7577b38538067a1d
OdmarOliv <odmaroliv@gmail.com>
2025-03-18 13:36:11 -0700

    [UPDATE] Se agrega true como valor por defecto al crear un nuermo en la configuracion de llamdas entrantes y salientes

---
+++ Migrations/20250318203432_AgregarConfiguracionLlamadasTrue.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TelefonicaEmpresaria.Migrations
+{
+    /// <inheritdoc />
+    public partial class AgregarConfiguracionLlamadasTrue : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<bool>(
+               name: "LlamadasSalientes",
+               table: "NumerosTelefonicos",
+               type: "boolean",
+               nullable: false,
+               defaultValue: true);
+
+            migrationBuilder.AlterColumn<bool>(
+                name: "LlamadasEntrantes",
+                table: "NumerosTelefonicos",
+                type: "boolean",
+                nullable: false,
+                defaultValue: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<bool>(
+                name: "LlamadasSalientes",
+                table: "NumerosTelefonicos",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AlterColumn<bool>(
+                name: "LlamadasEntrantes",
+                table: "NumerosTelefonicos",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+        }
+    }
+}
