7149aca1bb49a3aedf6a7bc92c1e53afa196f02a
OdmarOliv <odmaroliv@gmail.com>
2025-03-18 12:53:03 -0700

    update migraciones

---
+++ Migrations/20250318195232_AgregarConfiguracionLlamadas.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TelefonicaEmpresaria.Migrations
+{
+    /// <inheritdoc />
+    public partial class AgregarConfiguracionLlamadas : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "LlamadasEntrantes",
+                table: "NumerosTelefonicos",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "LlamadasSalientes",
+                table: "NumerosTelefonicos",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "LlamadasEntrantes",
+                table: "NumerosTelefonicos");
+
+            migrationBuilder.DropColumn(
+                name: "LlamadasSalientes",
+                table: "NumerosTelefonicos");
+        }
+    }
+}
